9672a1cf4b12709ecc8ac457e7fe1cc8a6c8fa55
Anthony Pardo <pardo.anthony@gmail.com>
2019-12-18 20:00:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -13,3 +13,15 @@ def sort_array_desc(arr)
     end 
   end
 end
+
+def sort_array_char_count(arr) 
+  arr.sort do |a, b|
+    if a.length == b.length 
+      0 
+    elsif a.length < b.length 
+      -1 
+    else 
+      1 
+    end
+  end
+end
